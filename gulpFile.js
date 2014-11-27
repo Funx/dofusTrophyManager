cce671f35ab3f9f47aa947b1f847c642703dce3d
Funx <geraud.henrion@gmail.com>
2014-11-27 17:17:54 +0100

    sass implementation - phase 2

--- gulpFile.js
+++ gulpFile.js
@@ -1,26 +1,28 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
+var reload      = browserSync.reload;
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
   gulp.src('./public/scss/app.scss')
-  .pipe(sass({
-    // includePaths: require('node-bourbon').with('other/path', 'another/path')
-    // - or -
-    includePaths: require('node-bourbon').includePaths
-  }))
-  .pipe(gulp.dest('path/to/output.css'));
+  .pipe(sass({includePaths: require('node-bourbon').includePaths}))
+  .pipe(gulp.dest('./public/css/'))
+  .pipe(reload({stream:true}));
 });
 
 gulp.task('browser-sync', function() {
     browserSync({
         proxy: "localhost:8080",
-        files:"./public/**/*",
+        files:["./public/**/*.html",
+        "./public/css/*.css",
+        "./public/scripts/**/*.js",
+        "./public/images/**/*"],
         notify: false
     });
     browserSync.reload();
 });
 
 gulp.task('default',['sass','browser-sync'],function(){
+  gulp.watch("./public/scss/**/*.scss", ["sass",browserSync.reload]);
   browserSync.reload();
 });
